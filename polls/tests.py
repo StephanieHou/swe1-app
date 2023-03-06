6a612a5e18751904bfa1bdd3122d2b40725c7aa4
Stephanie Hou <stephaniehou@cpe-68-174-37-33.nj.res.rr.com>
2023-03-05 19:27:38 -0500

    Black Formatting

--- polls/tests.py
+++ polls/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
+
 class PollsViewsTestCase(TestCase):
     def test_index(self):
-        resp = self.client.get('/polls/')
+        resp = self.client.get("/polls/")
         self.assertEqual(resp.status_code, 200)
